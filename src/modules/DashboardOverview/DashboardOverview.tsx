a1e91c41cff4835865f519c041a72710ea1d9a47
felixiho <okekefelix1@gmail.com>
2022-01-30 07:24:15 +0100

    buttons

--- src/modules/DashboardOverview/DashboardOverview.tsx
+++ src/modules/DashboardOverview/DashboardOverview.tsx
@@ -1,4 +1,5 @@
-import { Box } from "@chakra-ui/layout";
+import { Box, Text } from "@chakra-ui/layout";
+import CustomButton from "../../components/Button";
 import CompanyCard from "../../components/CompanyCard";
 
 const DashboardOverview = () => {
@@ -16,12 +17,22 @@ const DashboardOverview = () => {
       <h1>Dashboard</h1>
 
       <Box width="275px" m={10}>
-      <CompanyCard
-        companyName="Microphone Company"
-        users={29}
-        leads={223}
-        revenue={90}
-      />
+        <CompanyCard
+          companyName="Microphone Company"
+          users={29}
+          leads={223}
+          revenue={90}
+        />
+
+        <br />
+        <CustomButton>Send Contact </CustomButton>
+        <br />
+        <br />
+        <br />
+        <CustomButton variant="outline">Send Contact </CustomButton>
+        <br />
+        <br />
+        <CustomButton variant="gray">Send Contact </CustomButton>
 
       </Box>
     </Box>
