6b6da7f58cd33a518c75c97136565686dc39de4f
Chinoso Jude <chinosojude@Chinosos-MacBook-Pro.local>
2022-02-12 10:55:27 +0100

    add redux to code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,11 @@ import '../styles/globals.css';
 import '../styles/404.css';
 import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
-import theme from '../styles/theme';
 import Fonts from 'utils/Fonts';
+import { axiosInstance } from 'utils/helpers';
+import theme from '../styles/theme';
+import { wrapper } from '../store';
+import { APP_NAME, NAV_NAME } from 'utils/constants/pageDataConstants';
 
 function QuikInfluenceApp({ Component, pageProps }: AppProps) {
   return (
@@ -14,4 +17,33 @@ function QuikInfluenceApp({ Component, pageProps }: AppProps) {
   );
 }
 
-export default QuikInfluenceApp;
+QuikInfluenceApp.getInitialProps = async () => {
+  // console.log(req, res);
+
+  // const cookies = new Cookies(req, res);
+  // const staicCoookie = cookies.get('jayne-static');
+
+  // console.log(staicCoookie);
+
+  if (typeof window === 'undefined') {
+    let nav:any;
+
+    try {
+      const nav = await axiosInstance.get(
+        `/content?resource=${APP_NAME}&page=${NAV_NAME}`
+      );
+    } catch (err) {
+      //
+    }
+
+    return {
+      pageProps: {
+        nav: nav.data.data,
+      },
+    };
+  }
+
+  return {};
+};
+
+export default wrapper.withRedux(QuikInfluenceApp);
