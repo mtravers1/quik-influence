a2e53c51625b40fc0112cf00996cffaf7e2148c7
felixiho <okekefelix1@gmail.com>
2022-02-12 10:36:31 +0100

    setup react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,24 @@ import '../styles/globals.css';
 import '../styles/404.css';
 import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from "react-query";
 import theme from '../styles/theme';
 import Fonts from 'utils/Fonts';
 
 function QuikInfluenceApp({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+
+      }
+    }
+  });
   return (
     <ChakraProvider theme={theme}>
       <Fonts />
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </ChakraProvider>
   );
 }
