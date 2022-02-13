3f70a5eb6590c0acd8db851e1f968794e09afee2
felixiho <okekefelix1@gmail.com>
2022-02-13 08:49:18 +0100

    email conversions widget

--- src/modules/DashboardOverview/DashboardOverview.tsx
+++ src/modules/DashboardOverview/DashboardOverview.tsx
@@ -13,6 +13,7 @@ import OverviewSkeletonLoaders from "components/SkeletonLoaders";
 import React from "react";
 import { cardThemeColor } from "utils/constants/colorConstants";
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import EmailConversions from "./charts/EmailConversions";
 
 const DashboardOverview = () => {
   const { colorMode } = useColorMode();
@@ -29,36 +30,7 @@ const DashboardOverview = () => {
             gap={4}
           >
             <GridItem rowSpan={2} colSpan={1}>
-              <Flex
-                padding={[6, 8]}
-                background={cardThemeColor[colorMode]}
-                borderRadius="8px"
-                flexDirection="column"
-              >
-                <Flex justify="space-between" mb={5}>
-                  <Flex
-                    w="75%"
-                    maxW="fit-content"
-                    justify="space-between"
-                    alignItems="center"
-                  >
-                    <Heading size="md">Email Conversions</Heading>
-                    <Divider mx={4} h="60%" orientation="vertical" />
-                    <Text fontSize="md">March 2020</Text>
-                  </Flex>
-                  <IconButton
-                    aria-label="menu options"
-                    variant="ghost"
-                    icon={<FontAwesomeIcon icon={faEllipsisH as IconProp} />}
-                  />
-                </Flex>
-                <Box w="500px">
-                  <Image
-                    src="https://46gyn61z4i0t1u1pnq2bbk2e-wpengine.netdna-ssl.com/wp-content/uploads/2018/10/pie-chart.png"
-                    alt="Dan Abramov"
-                  />
-                </Box>
-              </Flex>
+              <EmailConversions />
             </GridItem>
             <GridItem colSpan={1}>
               <Flex
@@ -86,7 +58,7 @@ const DashboardOverview = () => {
                 </Flex>
                 <Box>
                   <Image
-                    src="https://46gyn61z4i0t1u1pnq2bbk2e-wpengine.netdna-ssl.com/wp-content/uploads/2018/10/pie-chart.png"
+                    src="https://46gyn61z4i0t1u1pnq2bbk2e-wspengine.netdna-ssl.com/wp-content/uploads/2018/10/pie-chart.png"
                     alt="Dan Abramov"
                   />
                 </Box>
--- src/utils/helpers.tsx
+++ src/utils/helpers.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const baseurl =
   process.env.NODE_ENV === 'development'
-    ? 'http://localhost:4000'
+    ? 'https://quik-influence.herokuapp.com'
     : 'https://quik-influence.herokuapp.com';
 
 export const axiosInstance = axios.create({
