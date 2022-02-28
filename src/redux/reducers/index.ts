c26749bc661cce6a041db34e1553fbdf3df8271a
cvjude <jjchinosoviolet@gmail.com>
2022-02-28 07:30:46 +0100

    remove unnecssary code

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import nav from './nav';
-import campaigns from './campaign';
 
 const reducers = combineReducers({
   nav,
-  campaigns,
 });
 
 export default reducers;
