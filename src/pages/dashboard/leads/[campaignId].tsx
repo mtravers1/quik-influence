c838361be17eac76f8976b11def9d6a45480d6c0
Gabriel Micah <gabrielsuave17@gmail.com>
2022-03-13 18:22:51 +0100

    Create [campaignId].tsx

---
+++ src/pages/dashboard/leads/[campaignId].tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useRouter } from "next/router";
+import MainContent from "components/MainContent";
+import Filters from "components/LeadsPageFilters";
+import { TablePageLoader } from "components/SkeletonLoaders";
+import { getCampaignLeads } from "redux/actions/campaigns";
+import Leads from "modules/Leads";
+
+const CampaignsLeads = () => {
+  const { leads } = useSelector((state: any) => state.campaigns);
+  const router = useRouter();
+  const dispatch = useDispatch();
+
+  const campaignId = router.query.campaignId as string;
+  const page = router.query.page as string;
+  const socialColumns = router.query.sc as string;
+
+  const campaignsLeads = leads[campaignId];
+  const [loading, setLoading] = useState(!campaignsLeads);
+
+  const fetchCampaignsLeads = async () => {
+    setLoading(true);
+
+    await dispatch(getCampaignLeads(campaignId, page));
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchCampaignsLeads();
+  }, [page]);
+
+  return loading ? (
+    <MainContent>
+      <TablePageLoader />
+    </MainContent>
+  ) : (
+    <MainContent filter={<Filters />}>
+      <Leads leads={campaignsLeads} socialColumns={socialColumns?.split(",")} />
+    </MainContent>
+  );
+};
+
+export default CampaignsLeads;
